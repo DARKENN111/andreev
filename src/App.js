10d35e2aa1f13311638d19d19060b4467135ba2e
Danila <43791755+DARKENN111@users.noreply.github.com>
2020-09-15 02:18:50 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,62 +19,39 @@ import '@vkontakte/vkui/dist/vkui.css';
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            user: null
-        };
-    }
+		this.state = {
+			user: null
+		};
+	}
 
-    componentDidMount() {
-        bridge.send('VKWebAppGetUserInfo', {})
-            .then((user) => {
-                this.setState({ user });
-            });
-    }
+	componentDidMount() {
+		bridge.send('VKWebAppGetUserInfo', {})
+			.then((user) => {
+				this.setState({ user });
+			});
+	}
 
-    getImage = async () => {
+	getImage = async () => {
         const image = document.getElementById('url').value;
 
-        fetch(image, {
-        method: 'GET',
-        headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-            },
-        body: undefined,
-        referrer: "",
-        referrerPolicy: "no-referrer-when-downgrade", 
-        mode: "no-cors", 
-        credentials: "same-origin",
-        cache: "default", 
-        redirect: "follow", 
-        integrity: "", 
-        keepalive: false, 
-        signal: undefined,
-        })
-    .then((response) => {
-      return response.text();
-    })
-    .then((data) => {  
-      this.state = {
-            image: JSON.parse(data)
-        };
-    });
-
         console.log({image});
-    };
 
-    render() {
-        const { user, image } = this.state;
+        if (image) {
+			this.setState({ image });
+		}
+	};
 
-        return (
+	render() {
+		const { user, image } = this.state;
+
+		return (
             <View activePanel="main">
                 <Panel id="main">
                     <PanelHeader>
-                        {
+						{
                             user ?
                                 <PanelHeaderContent
                                     status="VK Apps Image Example"
@@ -82,36 +59,36 @@ class App extends React.Component {
                                 >
                                     { user.first_name }
                                 </PanelHeaderContent>
-                                :
-                                'VK Apps Image Example'
-                        }
+								:
+								'VK Apps Image Example'
+						}
                     </PanelHeader>
 
                     <Group header={ <Header mode="secondary">Задание</Header> }>
-                        <Div>
-                            У нас есть мини-приложение, которое имеет возможность загружать изображения по внешней ссылке. Увы, в приложении допущена ошибка, из-за чего удаленный сервер получает параметры запуска пользователей, которые вставляют ссылку. Нужно объяснить почему так происходит и исправить ситуацию красиво и лаконично.
-                        </Div>
-                        <Div>
-                            Для облегчения тестирования можно использовать картинку <Text weight="semibold">https://service.pavel.im/image</Text> (good code — все круто, bad code — сервер получил параметры запуска)
-                        </Div>
+						<Div>
+							У нас есть мини-приложение, которое имеет возможность загружать изображения по внешней ссылке. Увы, в приложении допущена ошибка, из-за чего удаленный сервер получает параметры запуска пользователей, которые вставляют ссылку. Нужно объяснить почему так происходит и исправить ситуацию красиво и лаконично.
+						</Div>
+						<Div>
+                            Для облегчения тестирования можно использовать картинку <Text weight="semibold">https://service.pavel.im/image</Text> (good code — все круто, bad code — сервер получил параметры запусука)
+						</Div>
 
                         <FormLayout>
                             <Input type="text" id="url" />
 
-                            <Button size="xl" onClick={ this.getImage }>Загрузить</Button>
+                            <Button size="xl" onClick={ this.image }>Загрузить</Button>
                         </FormLayout>
 
-                        {
-                            !!image &&
-                            <Div style={{ textAlign: 'center' }}>
+						{
+							!!image &&
+							<Div style={{ textAlign: 'center' }}>
                                 <img src={ image } alt="remote file" />
-                            </Div>
-                        }
+							</Div>
+						}
                     </Group>
                 </Panel>
             </View>
-        );
-    }
+		);
+	}
 
 }
 
